62a9d198e508f720f1cf6217c9573839e890becf
yancarlodev <yancarlodc@gmail.com>
2022-12-12 15:47:48 -0300

    wip: first part

--- users/models.py
+++ users/models.py
@@ -3,5 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    email = models.EmailField(max_length=127, unique=True, error_messages={'unique': 'This field must be unique.'})
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
--- users/serializers.py
+++ users/serializers.py
@@ -3,24 +3,14 @@ from rest_framework.validators import UniqueValidator
 from .models import User
 
 
-class UserSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    username = serializers.CharField(
-        validators=[
-            UniqueValidator(
-                queryset=User.objects.all(),
-                message="A user with that username already exists.",
-            )
-        ],
-    )
-    email = serializers.EmailField(
-        validators=[UniqueValidator(queryset=User.objects.all())],
-    )
-    password = serializers.CharField(write_only=True)
-    first_name = serializers.CharField(max_length=50)
-    last_name = serializers.CharField(max_length=50)
-    is_superuser = serializers.BooleanField(read_only=True)
-
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'first_name', 'last_name', 'id', 'is_superuser']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+        
     def create(self, validated_data: dict) -> User:
         return User.objects.create_superuser(**validated_data)
 
