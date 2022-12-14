3484bda1d83eca851957a76f110fee82d10810ca
yancarlodev <yancarlodc@gmail.com>
2022-12-14 11:07:56 -0300

    feat: lesson 3 part

--- songs/views.py
+++ songs/views.py
@@ -6,31 +6,15 @@ from .models import Song
 from rest_framework.pagination import PageNumberPagination
 from .serializers import SongSerializer
 from albums.models import Album
+from rest_framework.generics import ListCreateAPIView
 
 
-class SongView(APIView, PageNumberPagination):
+class SongView(ListCreateAPIView, PageNumberPagination):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = SongSerializer
+    queryset = Song.objects.all()
 
-    def get(self, request, pk):
-        """
-        Obtençao de musicas
-        """
-        songs = Song.objects.filter(album_id=pk)
-
-        result_page = self.paginate_queryset(songs, request)
-        serializer = SongSerializer(result_page, many=True)
-
-        return self.get_paginated_response(serializer.data)
-
-    def post(self, request, pk):
-        """
-        Criaçao de musica
-        """
-        album = get_object_or_404(Album, pk=pk)
-
-        serializer = SongSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+    def perform_create(self, serializer):
+        album = get_object_or_404(Album, pk=self.kwargs['pk'])
         serializer.save(album=album)
-
-        return Response(serializer.data, status.HTTP_201_CREATED)
